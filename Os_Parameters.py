a2a655d4ae7a0282bf1b0fb084fd694601811b93
An10nim4ik <121002188+An10nim4ik@users.noreply.github.com>
2024-04-18 15:37:33 +0500

    Update Os_Parameters.py

--- Os_Parameters.py
+++ Os_Parameters.py
@@ -1,8 +1,9 @@
 import platform
 import psutil
 import socket
-from colorama import init, Fore, Style
+from colorama import init, Fore
 import getpass
+import os
 
 # Retrieve OS Name and Version
 os_name = platform.system()
@@ -32,3 +33,19 @@ print(Fore.MAGENTA + "Memory (GB): {} GB".format(memory_info))
 print(Fore.CYAN + "Available Disk Space (GB): {:.2f} GB".format(disk_info))
 print(Fore.YELLOW + "Current User: {}".format(current_user))
 
+try:
+    # Attempt to retrieve memory information
+    memory_info = round(psutil.virtual_memory().total / (1024.0 ** 3), 2)
+    print(Fore.LIGHTBLACK_EX + "Memory (GB): {} GB".format(memory_info))
+except Exception as e:
+    print("Error retrieving memory information:", e)
+
+# Retrieve and display environmental variables
+env_vars = os.environ
+print(Fore.LIGHTYELLOW_EX + "Environment Variables:")
+for var, value in env_vars.items():
+    print(f"{var}: {value}")
+
+# Retrieve CPU Usage
+cpu_usage = psutil.cpu_percent(interval=1)
+print(Fore.LIGHTCYAN_EX + "CPU Usage: {}%".format(cpu_usage))
